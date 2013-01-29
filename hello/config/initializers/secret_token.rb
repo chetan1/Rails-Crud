0979decb24f1eb70f580361dfd7329dd215215e2
Ubuntu <bitnami@domU-12-31-39-06-7C-3F.(none)>
2013-01-29 03:00:38 +0000

    Initial commit

---
+++ hello/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello::Application.config.secret_token = '90a753b2e4d5ba88c63bdebeec21e1ef37516ef9e33bb7c825eb7d7963b97c739c3c3bb453d8e14505197eb8c86e226a3d7cb2cd326d1afe067a4ab6b27acc2f'
